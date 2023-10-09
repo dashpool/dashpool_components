2f61080340f906c8c8b41eca1ea49761961118a3
VK <viktor@krueckl.de>
2023-10-09 22:02:24 +0200

    login poppup

--- src/ts/components/DashpoolProvider.tsx
+++ src/ts/components/DashpoolProvider.tsx
@@ -1,6 +1,7 @@
 // DashpoolContext.tsx
 import React, { createContext, useContext, useState, ReactNode, useEffect, useRef } from 'react';
 import { Toast } from 'primereact/toast';
+import { Dialog } from 'primereact/dialog';
 
 type AppInfo = {
   id: string,
@@ -51,6 +52,40 @@ const DashpoolProvider = (props: DashpoolProviderProps) => {
     setSharedData({ ...sharedData, ...newData });
   };
 
+
+  // /// LOGIN section
+  const [showLoginModal, setShowLoginModal] = useState(false);
+
+  const initLogin = () => {
+    // Get the current URL
+    const currentUrl = window.location.href;
+
+    // Construct the URL for the OAuth2 proxy login
+    const oauth2StartUrl = `/oauth2/start?rd=${encodeURIComponent(currentUrl)}`;
+
+    // Attempt to open a popup window for OAuth2 login
+    const popupWindow = window.open(oauth2StartUrl, 'OAuth2 Login', 'width=600,height=400');
+
+    if (popupWindow) {
+      // Add an event listener to handle when the popup window is closed
+      popupWindow.addEventListener('beforeunload', () => {
+        setShowLoginModal(false);
+      });
+    } else {
+      // If the browser blocks the popup, perform a redirect instead
+      window.location.href = oauth2StartUrl;
+    }
+  }
+
+
+  const onHide = () => {
+    setShowLoginModal(false);
+  };
+  // //END Login section
+
+
+
+
   useEffect(() => {
     // Add event listener for the message event
     const messageEventListener = (event: MessageEvent) => {
@@ -60,7 +95,11 @@ const DashpoolProvider = (props: DashpoolProviderProps) => {
       if (messageData.type === 'fetchError') {
         // Show PrimeReact Toast with the message
 
-        toast.current?.show({ severity: "warn", summary: messageData.message, detail: JSON.stringify(messageData), life: 3000 });
+        // if (messageData.staus == 401) {
+        //   setShowLoginModal(true);
+        // }
+
+        toast.current?.show({ severity: "warn", summary: messageData.message, detail: messageData.url, life: 3000 });
       }
     };
 
@@ -83,6 +122,22 @@ const DashpoolProvider = (props: DashpoolProviderProps) => {
 
       {/* PrimeReact Toast */}
       <Toast ref={toast} position="bottom-right" />
+
+      <Dialog
+        visible={showLoginModal}
+        onHide={onHide}
+        header="You are not authorized"
+        modal={true}
+        footer={
+          <div>
+            <button onClick={initLogin}>OK</button>
+            <button onClick={onHide}>Cancel</button>
+          </div>
+        }
+      >
+        Do you want to log in?
+      </Dialog> 
+
     </div>
   );
 };
