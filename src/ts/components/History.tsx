8a92d586c4cbe885cbfae04df2cbf4e9ec5795a9
VK <viktor@krueckl.de>
2023-10-14 17:17:45 +0200

    release

--- src/ts/components/History.tsx
+++ src/ts/components/History.tsx
@@ -90,7 +90,7 @@ const History = (props: HistoryProps) => {
         const id = generateUniqueId();
         const frame = internalNode["id"];
         const label = internalNode["label"];
-        const app_data = internalNode["app_data"]
+        const app_data = internalNode["app"]
 
 
         fetch("/backend/savelayout", {
@@ -116,7 +116,7 @@ const History = (props: HistoryProps) => {
             // Handle the response data here if needed
             updateSharedData({
               dragElement: {
-                id: id, type: "a", label: label, parent: "history", "app": internalNode.data, "frame": frame, layout: id
+                id: id, type: "a", label: label, parent: "history", "app": internalNode.app, "frame": frame, layout: id
               }
             });
           })
