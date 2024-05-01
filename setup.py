67702d37e6f82348fe4c2d755058e86ece52ce0f
VK <viktor@krueckl.de>
2024-05-01 17:19:51 +0200

    release

--- setup.py
+++ setup.py
@@ -7,14 +7,21 @@ with open('dashpool_components/package-info.json') as f:
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name=package_name,
+    url="https://github.com/dashpool",
     version=package["version"],
     author=package['author'],
     packages=[package_name],
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
+    long_description= long_description,
+    long_description_content_type="text/markdown",        
     install_requires=[],
     classifiers=[
         'Framework :: Dash',
