971806af1e1e751ef532daad3387655e12d92569
Viktor Krückl <viktor.krueckl@ams-osram.com>
2025-01-01 20:10:41 +0100

    release

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import json
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 with open('dashpool_components/package-info.json') as f:
@@ -16,7 +16,7 @@ setup(
     url="https://github.com/dashpool",
     version=package["version"],
     author=package['author'],
-    packages=[package_name],
+    packages=[p for p in find_packages() if p.startswith(package_name)],
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
